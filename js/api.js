3d392e09c68ca50729bc2285301462ad31b4a014
Peprah Obed Adjei <nanakwameadjeipeprah@gmail.com>
2024-05-03 20:34:58 +0530

    first commit

---
+++ js/api.js
@@ -0,0 +1,23 @@
+const api_key = "52d4fc2f870b24954f41c1e73f2ea520"
+
+export const fetchData = function (URL, callback) {
+    fetch(`${URL}&appid=${api_key}`).then(res => res.json()).then(data => callback(data))
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    geo(query) {
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
